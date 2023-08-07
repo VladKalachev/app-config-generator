305fcdb3f8f59fc9b3c2ac841a68a3bdec5c086e
Corvax <vlvete1990@yandex.ru>
2023-08-07 09:28:25 +0300

    chore: added interface for fn get keys

--- src/shared/lib/getKeys.ts
+++ src/shared/lib/getKeys.ts
@@ -1,5 +1,9 @@
 
+export interface IKeys{
+  [key: string]: any;
+}
+
 // Получение всех ключей у объекта
-export const getKeys = (obj: any) => Object.keys(obj).flatMap(k => Object(obj[k]) === obj[k] 
+export const getKeys = (obj: IKeys): string[] => Object.keys(obj).flatMap(k => Object(obj[k]) === obj[k] 
 ? [k, ...getKeys(obj[k])] 
 : k)
